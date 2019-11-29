d2f1369a314f6e179b081cb6b6da4a52fd00a1f5
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-11-29 17:29:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+static capitalize = (string) =>{
+    return string.CharAt(0).toUpperCase() + string.slice(1);
+}
+static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-']+/g, '');
+}
+static titleize(string){
+    const punj = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let cap = string.replace(/^.{1}/g, s[0].toUpperCase());
+    let arr = cap.split(' ');
+    let last = arr.map(function(word){
+      for(let i = 0; i < punj.length; i ++){
+        if(punj[i] === word){
+          return word.CharAt(0).toLowerCase() + word.slice(1);
+        }
+      }
+    });
+    return last.join(',');
+  }
 }
